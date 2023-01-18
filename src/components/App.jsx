852985fa66af2857d6024e903c19e742be8b0aba
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-01-19 00:33:27 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import userData from '../data/user';
+import statsData from '../data/data';
+import friendsData from '../data/friends';
+import transactionsData from '../data/transactions';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile data={userData} />;
+      <Statistics title="Upload stats" stats={statsData} />
+      <FriendList friends={friendsData} />
+      <TransactionHistory items={transactionsData} />
+    </>
   );
 };
